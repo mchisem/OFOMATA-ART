509d0fc36ee0371445f7c1909bd543b77c5a7e62
Maya Chisem <chisem.maya@gmail.com>
2020-10-27 21:46:12 -0700

    adding compressed image files and running app on local server

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const compression = require('compression')
-const express = require('express')
+const compression = require('compression');
+const express = require('express');
+
+const PORT = process.env.PORT || 9000;
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 
 // compressed compression function
-var app=express();function shouldCompress(s,e){return!s.headers["x-no-compression"]&&compression.filter(s,e)}app.use(compression({filter:shouldCompress}));
+app;function shouldCompress(s,e){return!s.headers["x-no-compression"]&&compression.filter(s,e)}app.use(compression({filter:shouldCompress}));
+
